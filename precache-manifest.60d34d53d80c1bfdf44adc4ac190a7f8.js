2d4320f79b0f5d7fd542e9d41f2fab7913712243
jenniferawesomecoder <dogdoglovernumerouno1985@gmail.com>
2020-09-20 15:36:31 +0530

    Updates

---
+++ precache-manifest.60d34d53d80c1bfdf44adc4ac190a7f8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e65fb6804cce3986a64a99a6231061",
+    "url": "/github-graphql-api/index.html"
+  },
+  {
+    "revision": "f453da3117c6404a6a5a",
+    "url": "/github-graphql-api/static/css/2.45eb3147.chunk.css"
+  },
+  {
+    "revision": "d1f117c6459a3853e5c8",
+    "url": "/github-graphql-api/static/css/main.7265b016.chunk.css"
+  },
+  {
+    "revision": "f453da3117c6404a6a5a",
+    "url": "/github-graphql-api/static/js/2.79ff4e48.chunk.js"
+  },
+  {
+    "revision": "b5901214077ea32a7534a1d4eff7c84a",
+    "url": "/github-graphql-api/static/js/2.79ff4e48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1f117c6459a3853e5c8",
+    "url": "/github-graphql-api/static/js/main.fc154fce.chunk.js"
+  },
+  {
+    "revision": "faaecccc1de5bb04884d",
+    "url": "/github-graphql-api/static/js/runtime-main.6e4c36cc.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/github-graphql-api/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/github-graphql-api/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/github-graphql-api/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/github-graphql-api/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/github-graphql-api/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- src/containers/App/constants.js
+++
@@ -1,3 +0,0 @@
-export const API_ROOT = 'https://api.github.com/graphql';
-export const GITHUB_ACCESS_KEY = '';
-export const LIBRARIES_ACCESS_KEY = '';
